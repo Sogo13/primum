c3d884671a1d3e816889fbccc1ca29ea6b9f25d5
Sogo13 <Backin90@yandex.ru>
2024-03-13 02:03:22 +0300

    fix

--- src/components/Header/HeaderBlock.vue
+++ src/components/Header/HeaderBlock.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="header">
-    <p>Header</p>
+    <div class="container">
+      <div class="flex_jc_sb_al_c mainHeader">
+        <p class="heading_1 logoColor">PRIMUM</p>
+        <MainSearch/>
+        <div class="flex_jc_sb_al_c">
+        <button class="createProjectBtn heading_5">Create Project</button>
+        <p class="logTxt heading_5">Log in</p>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
-<script setup>
+<script>
+import MainSearch from "@/components/Header/MainSearch.vue";
+export default {
+  components: {
+    MainSearch
+  },
+  data() {
+
+  }
+}
 
 </script>
 
 <style scoped lang="scss">
-
+.mainHeader{
+  padding-top: 24px ;
+}
+.logTxt{
+  margin-left: 16px;
+}
+.createProjectBtn{
+  padding: 8px 16px;
+border-right: 2px solid ;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import legacy from '@vitejs/plugin-legacy'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,10 +8,26 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/mixins.scss";`,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  }, server: {
+    port: 3000,
+    open: '/',
+  },
 })
